a58049f92d7aeeb0b1f5853c6b05400a07063634
Himanshu <himanshu0998@gmail.com>
2024-04-06 13:10:51 -0400

    Automated database creation if it does not exist already

--- database.py
+++ database.py
@@ -2,6 +2,7 @@
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy_utils import database_exists, create_database
 from config import Config
 import pymysql
 
@@ -15,6 +16,10 @@ MYSQL_DBNAME = db.MYSQL_DB
 DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PWD}@{MYSQL_HOST}/{MYSQL_DBNAME}'
 # print(DATABASE_URI)
 engine = create_engine(DATABASE_URI, echo=True)
+
+if not database_exists(engine.url):
+    create_database(engine.url)
+
 session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 SessionLocal = scoped_session(session_factory)
 
